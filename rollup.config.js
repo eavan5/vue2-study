be16e6a2c0e080943f0fbc4cc179a5d578793734
wumao <webwwy@gmail.com>
2022-03-30 22:39:05 +0800

    feat: 配置rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './dist/vue.js',
+    name: 'Vue', // 给你全局添加一个全局属性
+    format: 'umd', // esm  commonjs模块 iife自执行函数  umd(commonjs amd)
+    sourcemap: true
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
+}
